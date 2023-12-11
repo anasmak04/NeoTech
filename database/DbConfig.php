2854e5f14fa94d3a6c2fc2eef8721a82a8646e7e
anasmak04 <anasdev8@gmail.com>
2023-12-11 12:17:36 +0100

    Added a connection with mysqli

--- database/DbConfig.php
+++ database/DbConfig.php
@@ -3,4 +3,31 @@
 class DbConfig
 {
 
+    protected $username = "root";
+    protected $password = "";
+    protected $dbname = "NeoTech";
+    protected $servername = "localhost";
+    protected $db;
+
+    public function __construct(){
+
+        $this->db = new mysqli($this->servername,$this->username,$this->password,$this->dbname);
+
+        if($this->db->connect_error){
+            die("connection failed");
+        }
+
+        else {
+            echo "connection work";
+        }
+
+    }
+
+
+
+
+
+
+
+
 }
