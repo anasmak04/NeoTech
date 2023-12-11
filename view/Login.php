bbbd9aa2bcfd38a59f52ef8d400dbc16f354f8a6
anasmak04 <anasdev8@gmail.com>
2023-12-11 22:35:34 +0100

    Added public for css folder styling

--- view/Login.php
+++ view/Login.php
@@ -2,8 +2,22 @@
 ?>
 
 
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+    <link rel="stylesheet" href="../public/css/styles.css">
+</head>
+<body>
+<h2>Login Authentication</h2>
 <form action="../controller/LoginController.php" method="post">
     <input type="text" name="username" placeholder="username" />
     <input type="password" name="password" placeholder="password" />
     <button name="submit">save</button>
 </form>
+
+</body>
+</html>
--- view/Register.php
+++ view/Register.php
@@ -1,11 +1,21 @@
 <?php
 ?>
 
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Register</title>
+        <link rel="stylesheet" href="../public/css/styles.css">
+    </head>
+    <body>
+    <h2>Register Authentication</h2>
 
-<form action="../controller/RegisterController.php" method="post">
-    <input type="text" name="username" placeholder="username" />
-    <input type="text" name="fullname" placeholder="fullname" />
-    <input type="password" name="password" placeholder="password" />
-    <input type="password" name="confirmPwd" placeholder="confirmPwd" />
-    <button name="submit">save</button>
-</form>
+    <form action="../controller/RegisterController.php" method="post">
+        <input type="text" name="username" placeholder="username" />
+        <input type="text" name="fullname" placeholder="fullname" />
+        <input type="password" name="password" placeholder="password" />
+        <input type="password" name="confirmPwd" placeholder="confirmPwd" />
+        <button name="submit">save</button>
+
+    </body>
+    </html>
